0874e2818e0185acb302536a9b71392083959849
unknown <andresucero241093@gmail.com>
2021-01-20 13:44:21 -0430

    flex layout added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 const modules: Array<any> =[
-	MaterialModule
+	MaterialModule,
+  FlexLayoutModule
 ];
 
 const components: Array<any> =[
-
+  FooterComponent, 
+  HeaderComponent, 
+  SidebarComponent
 ];
 
 @NgModule({
-  declarations: [components, FooterComponent, HeaderComponent, SidebarComponent],
+  declarations: [components],
   imports: [
     CommonModule,
     modules
